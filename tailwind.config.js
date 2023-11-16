bdab7673d0d279458512b84159350d293f73d799
mc-github2022 <mark.bumagat@frontlinebusiness.com.ph>
2023-11-16 12:27:53 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+    colors: {
+      'primary' : '#039759',
+      'white' : '#ffffff',
+      'Dark' : '#09141A',
+    },
+    fontFamily: {
+      'dmSans' : ['DM Sans'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
