ea22286942b0b865393d700788764f1a554ec87e
monmonplaza <monmon.plaza@gmail.com>
2023-12-10 21:43:15 +0800

    123

--- dist/script.js
+++ dist/script.js
@@ -1,3 +1,6 @@
+AOS.init();
+
+// TOGGLE MENU
 const toggleMenu = document.querySelector(".toggle__menu");
 const mainNav = document.querySelector(".main__nav");
 const body = document.querySelector("body");
@@ -7,3 +10,26 @@ toggleMenu.addEventListener("click", () => {
   mainNav.classList.toggle("open");
   body.classList.toggle("open");
 });
+
+// MATERIALS
+
+const materialBtns = document.querySelectorAll(".material-btn");
+const materialContent = document.querySelectorAll(".material-content");
+
+materialBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    removeAllActive();
+    btn.classList.add("active");
+    document.getElementById(`content-${btn.id}`).classList.add("active");
+  });
+});
+
+function removeAllActive() {
+  materialBtns.forEach((item) => {
+    item.classList.remove("active");
+  });
+
+  materialContent.forEach((item) => {
+    item.classList.remove("active");
+  });
+}
